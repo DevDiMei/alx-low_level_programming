8dcb6620cc9bc96a40269d5546fdac788dd062a5
DevDiMei <meidecenandco@gmail.com>
2022-10-05 12:27:10 -0700

    Working with argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - multiplies two numbers
+ *
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int product = 0;
+int x, y;
+if (argc == 3)
+{
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+product = x * y;
+printf("%d\n", product);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
